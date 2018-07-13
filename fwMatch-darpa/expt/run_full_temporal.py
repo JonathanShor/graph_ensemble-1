f57591496860adecf49a46d88e8ae30f20a5d018
Jonathan Shor <11262246+JonathanShor@users.noreply.github.com>
2018-07-13 12:50:24 -0400

    Start of run_full_temporal.py
    One script to train the model on the data, create the shuffled versions of the data, train models on the shuffles, and extract the ensemble neuron IDs.

---
+++ fwMatch-darpa/expt/run_full_temporal.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import time
+import sys
+import os
+import shutil
+import subprocess
+
+EXPT_NAME = "temporal";
+MODEL_TYPE = "loopy";
+DATA_DIR = "~/data/";
+USER = "jds2270";
+EMAIL = "jds2270@columbia.edu";
+
+TRAIN_TEMPLATE_FOLDER_NAME = "{}_template".format(EXPT_NAME)
+SHUFFLE_TEMPLATE_FOLDER_NAME = # TODO
+
+
+def check_templates():
+    """Make template folders from master templates if they do not exist already
+    """
+    # Check train
+    if not os.path.exists(TRAIN_TEMPLATE_FOLDER_NAME):
+        shutil.copytree("temporal_template", TRAIN_TEMPLATE_FOLDER_NAME)
+
+    # TODO: Check stuffle
+
+
+def setup_train(condition_names):
+    for name in condition_names:
+        data_file = "{}_{}".format(EXPT_NAME, name)
+        experiment = "{}_{}_{}".format(EXPT_NAME, name, MODEL_TYPE)
+        os.mkdir(experiment)
+
+
+
+
+if __name__ == '__main__':
+    start_time = time.time()
+    conditions = sys.argv[1:]
+    if conditions:
+        check_templates()
+        setup_train(conditions)
+    else
+        raise TypeError("At least one condition name must be passed on the command line.\n")
+
+    print("Total run time: {0:.2f} seconds".format(time.time() - start_time))
