489a385ff1c20c0c52eb2b5bae1da82b2ea09ea8
Jonathan Shor <11262246+JonathanShor@users.noreply.github.com>
2018-07-19 15:22:55 -0400

    fixup! Continue run_full_temporal -- SQUASH THIS

--- fwMatch-darpa/expt/run_full_temporal.py
+++ fwMatch-darpa/expt/run_full_temporal.py
@@ -38,13 +38,14 @@ def setup_exec_train_model(condition_names):
         condition_names ([str]): List of condition names to setup.
     """
     for condition in condition_names:
-        data_file = "{}_{}".format(EXPT_NAME, condition)
+        data_file = "{}_{}.mat".format(EXPT_NAME, condition)
         experiment = "{}_{}_{}".format(EXPT_NAME, condition, MODEL_TYPE)
         print("Copying {} to {}".format(TRAIN_TEMPLATE_FOLDER_NAME, experiment))
         shutil.copytree(TRAIN_TEMPLATE_FOLDER_NAME, experiment)
 
         with open("{}{}write_configs_for_loopy.m".format(experiment, os.sep), 'w') as f:
             f.write("create_config_files( ...\n")
+            f.write("    'datapath', '{}{}',\n".format(DATA_DIR, data_file))
             f.write("    'experiment_name', '{}', ...\n".format(experiment))
             f.write("    'email_for_notifications', '{}', ...\n".format(EMAIL))
             f.write("    'yeti_user', '{}', ...\n".format(USER))
@@ -65,31 +66,6 @@ def setup_exec_train_model(condition_names):
             f.write("    'time_span', 2);\n")
         f.closed
         print("done writing write_configs_for_loopy.m")
-
-        # print("file: get_real_data.m\n")
-        # with open("{}{}get_real_data.m".format(experiment, os.sep), 'w') as f:
-        #     f.write("function [data,variable_names, stimuli] = get_real_data()\n")
-        #     f.write("load(['".$DATA_DIR."' ...\n")
-        #     f.write("          '".$DATA_FILE.".mat']);\n")
-        #     f.write("fprintf('Loaded: %s\\n', ['".$DATA_DIR."' ...\n".
-        #         "'".$DATA_FILE.".mat']);\n")
-        #     f.write("%data is time_frames by number_of_neurons\n")
-        #     f.write("data = full(data);\n")
-        #     f.write("N = size(data,2);\n")
-        #     f.write("fprintf('data is : %d, %d\\n', size(data,1), size(data,2));\n")
-        #     f.write("variable_names = {};\n")
-        #     f.write("for i = 1:N\n")
-        #     f.write("\tvariable_names(end+1) = {int2str(i)};\n")
-        #     f.write("end\n")
-        #     f.write("if exist('stimuli', 'var') == 1\n")
-        #     f.write("    stimuli = full(stimuli);\n")
-        #     f.write("    fprintf('stimuli is : %d, %d\\n', size(stimuli,1), size(stimuli,2));\n")
-        #     f.write("else\n")
-        #     f.write("    stimuli = [];\n")
-        #     f.write("end\n")
-        #     f.write("end\n")
-        # f.closed
-        # print "done writing get_real_data.m\n";
 
         curr_dir = os.getcwd()
         print("curr_dir = {}.".format(curr_dir))
